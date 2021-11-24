9b9f79be0337dd204e029dac9f45bf13c486a11b
Saul Andrade <saul.a.andrade@gmail.com>
2021-11-24 15:02:54 -0300

    fixed burguer button css selectors

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
 
 import Layout from './components/Layout/Layout';
+import AboutPage from './pages/About'
+import ProjectsPage from './pages/Projects'
+import ContactPage from './pages/Contact'
 
 function App() {
   return (
     <div>
       <Layout>
-        conteúdo
+        <Routes>
+          <Route path='/about' element={ <AboutPage /> } />  
+          <Route path='/projects' element={ <ProjectsPage /> } />
+          <Route path='/contact' element={ <ContactPage /> } />
+          <Route path='/' element={ <AboutPage /> } />  
+        </Routes>
       </Layout>
     </div>
   );
--- src/components/BurguerButton/BurguerButton.js
+++ src/components/BurguerButton/BurguerButton.js
@@ -7,7 +7,6 @@ import classes from './BurguerButton.module.css'
 const BurguerButton = () => {
   const [ buttonClasses, setButtonClasses ] = useState([ classes.BurguerButton ])
   const [ buttonActive, setButtonActive ] = useState (false)
-  const [ buttonHovered, setButtonHovered ] = useState(false)
 
   const toggleButtonHandler = () => {
     if (buttonClasses.includes(classes.ButtonActive)){
@@ -19,20 +18,14 @@ const BurguerButton = () => {
     setButtonActive(true)
 }
 
-  const spanBaseClass = buttonHovered ? classes.SpanHovered : classes.Span
-  
-
   return (
     <React.Fragment>
       <Curtain show={ buttonActive }/>
 
-      <div className={buttonClasses.join(' ')} 
-      onClick={toggleButtonHandler} 
-      onMouseEnter={()=>setButtonHovered(true)}
-      onMouseLeave={()=>setButtonHovered(false)}> 
-        <span className={buttonActive?classes.SpanActiveTop:spanBaseClass}></span>
-        <span className={buttonActive?classes.SpanActiveMiddle:spanBaseClass}></span>
-        <span className={buttonActive?classes.SpanActiveBottom:spanBaseClass}></span>
+      <div className={buttonClasses.join(' ')} onClick={toggleButtonHandler}> 
+        <span></span>
+        <span></span>
+        <span></span>
       </div>
     </React.Fragment>
   );
